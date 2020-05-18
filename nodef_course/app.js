26a158f3e98c01318ee031697896059d5c47ab92
imsandeepan <shondha.tara94@gmail.com>
2020-05-18 22:29:25 +0530

    This Challenge done

--- nodef_course/app.js
+++ nodef_course/app.js
@@ -1,2 +1,5 @@
 const fs = require('fs')
-fs.writeFileSync('notes.txt', 'I am Sandeepan.')
+//fs.writeFileSync('notes.txt', 'I am Sandeepan.')
+//challenge append text
+fs.appendFileSync('notes.txt', 'I am 25 year old.')
+
