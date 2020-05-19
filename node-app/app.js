ad3782da5d52e4f58ba2352f4799e4ce318f9ce5
imsandeepan <shondha.tara94@gmail.com>
2020-05-19 22:17:04 +0530

    Testing email validation

--- node-app/app.js
+++ node-app/app.js
@@ -1,3 +1,8 @@
+const validator = require('validator')
+console.log(validator.isEmail('shondh.tara94@gmail.com'))
+console.log(validator.isEmail('@gmail.com'))
+
+
 const gN = require('./notes.js')
 const name = gN()
 console.log(name)
