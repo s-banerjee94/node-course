27db9885ad004e3b0050e86d3ee979a2f696c439
imsandeepan <shondha.tara94@gmail.com>
2020-05-19 22:36:13 +0530

    Added Mobile Number validation

--- node-app/app.js
+++ node-app/app.js
@@ -1,12 +1,20 @@
 const validator = require('validator')
+
+//Email validation
 console.log('This Email is valid. ' + validator.isEmail('shondh.tara94@gmail.com'))
-console.log('This Email is valid. ' +validator.isEmail('@gmail.com'))
+console.log('This Email is valid. ' + validator.isEmail('@gmail.com'))
 
-console.log('This URL is valid. ' +validator.isURL('www.google.com'))
-console.log('This URL is valid. ' +validator.isURL('google.com'))
-console.log('This URL is valid. ' +validator.isURL('https:google.com'))
+//URL validation
+console.log('This URL is valid. ' + validator.isURL('www.google.com'))
+console.log('This URL is valid. ' + validator.isURL('google.com'))
+console.log('This URL is valid. ' + validator.isURL('https:google.com'))
+
+//Mobile number validation
+console.log('This URL is valid. ' + validator.isMobilePhone('+918436381333', ['en-IN']))
+console.log('This URL is valid. ' + validator.isMobilePhone('+91843638133', ['en-IN']))
 
 
+//My own function eport TEST
 const gN = require('./notes.js')
 const name = gN()
 console.log(name)
