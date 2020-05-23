e7027d605e859de6facad8b56f7ec46b6974b4c7
imsandeepan <shondha.tara94@gmail.com>
2020-05-23 21:59:19 +0530

    testing chalk

--- node-app/app.js
+++ node-app/app.js
@@ -1,24 +1,7 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const getNotes = require('./notes')
 
-//Email validation
-console.log('This Email is valid. ' + validator.isEmail('shondh.tara94@gmail.com'))
-console.log('This Email is valid. ' + validator.isEmail('@gmail.com'))
+const msg1 = chalk.bold.green('success!')
+const msg2 = chalk.underline.red('success!')
 
-//URL validation
-console.log('This URL is valid. ' + validator.isURL('www.google.com'))
-console.log('This URL is valid. ' + validator.isURL('google.com'))
-console.log('This URL is valid. ' + validator.isURL('https:google.com'))
-
-//Mobile number validation
-console.log('This URL is valid. ' + validator.isMobilePhone('+918436381333', ['en-IN']))
-console.log('This URL is valid. ' + validator.isMobilePhone('+91843638133', ['en-IN']))
-
-
-//My own function eport TEST
-const gN = require('./notes.js')
-const name = gN()
-console.log(name)
-
-// const add = require('./utils.js')
-// const sum = add(4, 5)
-// console.log(sum)
+console.log(msg1 + ' ' + msg2)
